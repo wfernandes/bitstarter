120b2f8d184a9478747d49d11544b594c32ff3ab
wfernandes <warren.f.fernandes@gmail.com>
2013-08-02 03:55:47 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Warren!');
+  // response.send('Hello Warren!');
+  fs.readFileSync('index.html', function(err, data){
+	if(err) throw err;
+	var buffer = new Buffer();
+	buffer.write(data);
+	response.send(buffer.toString('utf-8'));
+  });
 });
 
 var port = process.env.PORT || 5000;
